7643487453d7575d141c0bca1f5df15f12ec5ad9
Judith <j.r.lloydjones@gmail.com>
2018-09-26 16:21:01 +0000

    adds second test

--- lib/oddeven.rb
+++ lib/oddeven.rb
@@ -1,3 +1,7 @@
 def odd_or_even(number)
+  if number == 1
     'odd'
+  elsif number == 2
+    'even'
+  end
 end
--- spec/oddeven_spec.rb
+++ spec/oddeven_spec.rb
@@ -4,4 +4,7 @@ describe odd_or_even do
     it 'returns "odd" when passed 1' do
        expect(odd_or_even(1)).to eq 'odd' 
     end
+    it 'returns "even" when passed 2' do
+       expect(odd_or_even(2)).to eq 'even' 
+    end
 end
