fba0ec989e85961f0bcf278da180074c2b608916
Judith <j.r.lloydjones@gmail.com>
2018-09-27 14:53:33 +0000

    passed new rspec test to remove vowels from string

--- lib/remove_vowels.rb
+++ lib/remove_vowels.rb
@@ -1,11 +1,11 @@
-def test_for_vowels(letter)
-  if letter == "a" || letter == "e" || letter == "i" || letter == "o" || letter == "u"
-    true
-  else
-    false
+def remove_vowels(string)
+  vowels = ( "a" ||  "e" || "i" || "o" ||  "u")
+  no_vowels_string = string.chars.map do |letter| 
+    if letter != vowels
+     letter 
+    end
   end
+  no_vowels_string.join("")
 end
 
-def remove_vowels(string)
-  ""
-end
+
--- spec/remove_vowels_spec.rb
+++ spec/remove_vowels_spec.rb
@@ -1,25 +1,10 @@
 require 'remove_vowels.rb'
 
-describe 'test_for_vowels' do
- it 'returns true for the vowel a' do
-    expect(test_for_vowels("a")).to eq true
- end
- it 'returns true for all vowels' do
-    expect(test_for_vowels("e")).to eq true
-    expect(test_for_vowels("i")).to eq true
-    expect(test_for_vowels("o")).to eq true
-    expect(test_for_vowels("u")).to eq true
- end
- it 'returns false for non-vowels' do
-   expect(test_for_vowels("m")).to eq false
-   expect(test_for_vowels("n")).to eq false
-   expect(test_for_vowels("r")).to eq false
-   expect(test_for_vowels("s")).to eq false
- end
-end
-
 describe 'remove_vowels' do
  it 'returns empty string for the string a' do
    expect(remove_vowels("a")).to eq ("")
+ end
+ it 'returns b from string ab' do
+   expect(remove_vowels("ab")).to eq "b"
  end
 end
