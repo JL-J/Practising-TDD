388006920ee9ec995706f494177e52dff5c02aff
Judith <j.r.lloydjones@gmail.com>
2018-09-27 11:25:30 +0000

    addded fourth test and passed rspec

--- spec/smallestnumber_spec.rb
+++ spec/smallestnumber_spec.rb
@@ -10,4 +10,8 @@ describe 'smallest_number' do
     it 'returns the number 2 from the array' do
         expect(smallest_number([5, 2, 4])).to eq 2
     end 
+    it 'returns the number -4 from the array' do
+       expect(smallest_number([10, 0, 8, 100, 6, 27, -1, 75, -4, 89])).to eq (-4)
+    end
+
 end
