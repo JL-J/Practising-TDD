149772d6c01cc59f1a8595f08a6b3da31f64fef4
Caitlin Cooling <ccooling97@gmail.com>
2018-09-27 14:47:52 +0100

    Pass first test

--- spec/remove_vowels_spec.rb
+++ spec/remove_vowels_spec.rb
@@ -1,7 +1,7 @@
 require 'remove_vowels.rb'
 
-describe 'revome_vowels' do
+describe 'remove_vowels' do
  it 'recognises the letter a is a vowel and return true' do
     expect(remove_vowels("a")).to eq true
- end     
+ end
 end
