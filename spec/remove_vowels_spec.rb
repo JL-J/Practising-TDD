8269fb94a676e0d4972f48c78ef070b27f435053
Judith <j.r.lloydjones@gmail.com>
2018-09-27 13:53:41 +0000

    added second test and passed rspec

--- spec/remove_vowels_spec.rb
+++ spec/remove_vowels_spec.rb
@@ -1,7 +1,14 @@
 require 'remove_vowels.rb'
 
 describe 'remove_vowels' do
- it 'recognises the letter a is a vowel and return true' do
+ it 'return true for the vowel a' do
     expect(remove_vowels("a")).to eq true
  end
+ it 'return true for all vowels' do 
+    expect(remove_vowels("e")).to eq true
+    expect(remove_vowels("i")).to eq true
+    expect(remove_vowels("o")).to eq true
+    expect(remove_vowels("u")).to eq true
+ end
+ 
 end
