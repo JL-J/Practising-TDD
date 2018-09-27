02b83a738f65662e26b57a175dd2fd26ba6b0b53
Caitlin Cooling <ccooling97@gmail.com>
2018-09-27 14:58:18 +0100

    Third failing test

--- spec/remove_vowels_spec.rb
+++ spec/remove_vowels_spec.rb
@@ -4,11 +4,16 @@ describe 'remove_vowels' do
  it 'return true for the vowel a' do
     expect(remove_vowels("a")).to eq true
  end
- it 'return true for all vowels' do 
+ it 'return true for all vowels' do
     expect(remove_vowels("e")).to eq true
     expect(remove_vowels("i")).to eq true
     expect(remove_vowels("o")).to eq true
     expect(remove_vowels("u")).to eq true
  end
- 
+ it 'return false for non-vowels' do
+   expect(remove_vowels("m")).to eq false
+   expect(remove_vowels("n")).to eq false
+   expect(remove_vowels("r")).to eq false
+   expect(remove_vowels("s")).to eq false
+ end
 end
