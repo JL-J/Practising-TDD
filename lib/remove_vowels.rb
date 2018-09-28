f6418b8d95b603821108b2a489199afd93e20a1b
Caitlin Cooling <ccooling97@gmail.com>
2018-09-28 09:14:43 +0100

    Finish tests, methods and refactor

--- lib/remove_vowels.rb
+++ lib/remove_vowels.rb
@@ -1,11 +1,18 @@
+#
+# def remove_vowels(string)
+#   vowels = ["a", "e", "i", "o", "u"]
+#     no_vowels_string = string.chars.map do |letter|
+#       vowels.each do |vowel|
+#       if letter != vowel
+#         letter
+#       end
+#     end
+#     p no_vowels_string
+#   end
+# end
+
 def remove_vowels(string)
-  vowels = ( "a" ||  "e" || "i" || "o" ||  "u")
-  no_vowels_string = string.chars.map do |letter| 
-    if letter != vowels
-     letter 
-    end
-  end
-  no_vowels_string.join("")
+  p string.delete("aeiou")
 end
 
-
+remove_vowels("hello world")
--- spec/remove_vowels_spec.rb
+++ spec/remove_vowels_spec.rb
@@ -1,10 +1,19 @@
 require 'remove_vowels.rb'
 
 describe 'remove_vowels' do
- it 'returns empty string for the string a' do
+ it 'returns empty string when given a vowel' do
    expect(remove_vowels("a")).to eq ("")
+   expect(remove_vowels("e")).to eq ("")
+   expect(remove_vowels("i")).to eq ("")
+   expect(remove_vowels("o")).to eq ("")
+   expect(remove_vowels("u")).to eq ("")
  end
  it 'returns b from string ab' do
    expect(remove_vowels("ab")).to eq "b"
  end
+ it 'returns hello world as hll wrld' do
+   expect(remove_vowels("hello world")).to eq "hll wrld"
+ end
 end
+
+#
